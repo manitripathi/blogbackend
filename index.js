9de1891840428bdee2208954ecec31d8fd1958a5
yaqubk786 <yaqubk477@gmail.com>
2022-01-24 11:00:57 +0530

    backendonly

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import postRoutes from "./routes/route.js";
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/blog",postRoutes);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
